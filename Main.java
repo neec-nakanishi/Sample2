781b8b50ec6bd5eeae99360ed76dda1675ae65cb
neec-nakanishi <nakanishi@g.neec.ac.jp>
2024-11-28 14:26:27 +0900

    Main.java更新

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("こんにちは");
+        System.out.print("名前>");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("こんにちは、"+name+"さん");
     }
 }
